cc83ebf69fa9d691b6393f0920ea22bbaf3cf9ff
hideo-0103 <59080025+hideo-0103@users.noreply.github.com>
2020-02-27 19:37:22 +0900

    修正

--- app/assets/javascripts/machine.js
+++ app/assets/javascripts/machine.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   // 画像用のinputを生成する関数
   const buildFileField = (index)=> {
-    const html = `<div data-index="${index}" class="js-file_group card" >
+    const html = `<div class="js-file_group card" data-index="${index}" >
                   <i class="fas fa-times-circle js-remove"></i>
-                  <img src="https://jp.images-monotaro.com/Monotaro3/pi/full/mono_image_na.jpg" width="100%" height="100%" class="default btn img-btn">
+                  <img src="https://jp.images-monotaro.com/Monotaro3/pi/full/mono_image_na.jpg" width="100%" height="100%" class="default btn img-btn" >
                   <input class="js-file" type="file"
                   name="machine[images_attributes][${index}][image]"
                   id="machine_images_attributes_${index}_image">
@@ -30,26 +30,21 @@ $(document).ready(function() {
   fileIndex.splice(0, lastIndex);
   $('.hidden-destroy');
   $(document).on("click", ".img-btn", function(){
-    console.log('click');
-    let index = $(this).data('index');
-    console.log(index);
+    let index = $(this).parent().data('index');
     $(this).next('input').click();
   });
   
   $(document).on("change", ".js-file", function(e){
-    console.log('change');
-    const targetIndex = $(this).prev().data('index');
+    const targetIndex = $(this).parent().data('index');
     // ファイルのブラウザ上でのURLを取得する
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
     // 該当indexを持つimgタグがあれば取得して変数imgに入れる(画像変更の処理)
     if (img = $(`img[data-index="${targetIndex}"]`)[0] ) {
-      console.log('true')
       img.setAttribute('src', blobUrl);
     } else {  // 新規画像追加の処理
-      console.log('else');
       $(this).prev('.default').remove();
-      $(this).parent().append(buildImg(targetIndex, blobUrl));
+      $(this).parent().prepend(buildImg(targetIndex, blobUrl));
       // fileIndexの先頭の数字を使ってinputを作る
       $('#image-box').append(buildFileField(fileIndex[0]));
       fileIndex.shift();
@@ -60,7 +55,6 @@ $(document).ready(function() {
 
   $('#image-box').on('click', '.js-remove', function() {
     const targetIndex = $(this).parent().data('index')
-    console.log(targetIndex);
     // 該当indexを振られているチェックボックスを取得する
     const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
     // もしチェックボックスが存在すればチェックを入れる
